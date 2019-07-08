3593cb8d1a622f901b73bfc7e1b4fa2b2568bdee
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-08 17:37:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,13 +9,6 @@ class Scraper
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
     doc = Nokogiri::HTML(html)
-
-    # doc.css(".post").each do |post|
-    #   course = Course.new
-    #   course.title = post.css("h2").text
-    #   course.schedule = post.css(".date").text
-    #   course.description = post.css("p").text
-    # end
   end
 
   def get_courses
