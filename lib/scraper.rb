03e8a674788d847b17d211dc316170f882d10892
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-08 17:37:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -41,16 +41,7 @@ class Scraper
     end
   end
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+
 
 
 end
